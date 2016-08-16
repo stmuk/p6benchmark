163b3823128672e972ef1eb7baa17b5df0156d87
Steve Mynott <steve@tau.local>
2016-08-16 12:22:59 +0100

    virtualsue improvements

--- bench.go
+++ bench.go
@@ -12,40 +12,41 @@ import (
 
 func main() {
 
-	var num int64
-	var target []string
-	var lang string
-
-	if len(os.Args) > 3 {
-		num, _ = strconv.ParseInt(os.Args[1], 10, 64)
-		lang = os.Args[2]
-		target = os.Args[3:]
-	} else {
-		log.Fatal("number of iterations, lang, target program")
+	if len(os.Args) < 3 {
+		Usage()
 	}
 
-	var i int64 // XXX
+	repeatMax, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		Usage()
+	}
+
+	program := os.Args[2]
+	opts := os.Args[3:]
 
 	start := time.Now()
 
-	for i = 0; i < num; i++ {
+	for i := 0; i < repeatMax; i++ {
 
 		fmt.Printf("%d ", i)
 
-		out, err := exec.Command(lang, target...).Output()
-
-		if num == 1 {
-			fmt.Fprintf(os.Stderr, "%s\n", out)
-		}
-
+		out, err := exec.Command(program, opts...).Output()
 		if err != nil {
 			log.Fatal(err)
+		}
+
+		if repeatMax == 1 {
+			fmt.Fprintf(os.Stderr, "%s\n", out)
 		}
 
 	}
 
 	elapsed := time.Since(start).Seconds()
-	avg := elapsed / float64(num)
-	fmt.Printf("\n\"%s %s\" %d run(s) wallclock=%0.2f avg=%0.2f sec(s)\n", lang, strings.Join(target, " "), num, float64(elapsed), avg)
+	avg := elapsed / float64(repeatMax)
+	fmt.Printf("\n\"%s %s\" %d run(s) wallclock=%0.2f avg=%0.2f sec(s)\n", program, strings.Join(opts, " "), repeatMax, float64(elapsed), avg)
 
+}
+
+func Usage() {
+	log.Fatalf("Usage: %s <# iterations> <program path>\n", os.Args[0])
 }
